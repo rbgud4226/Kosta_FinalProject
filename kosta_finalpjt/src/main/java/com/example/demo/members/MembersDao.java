4e741ecd72008e423030fade12f8292f5e44a5e2
choiwanmin <choi.wm512@gmail.com>
2024-06-05 09:16:46 +0900

    add:member 패키지 사진 파일 업로드 및 승인 관련 코드 추가

--- kosta_finalpjt/src/main/java/com/example/demo/members/MembersDao.java
+++ kosta_finalpjt/src/main/java/com/example/demo/members/MembersDao.java
@@ -3,14 +3,26 @@ package com.example.demo.members;
 import java.util.ArrayList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.depts.Depts;
 import com.example.demo.users.Users;
+
+import jakarta.transaction.Transactional;
+import java.util.List;
+
 
 @Repository
 public interface MembersDao extends JpaRepository<Members, Integer> {
 	Members findByUserid(Users userid);
 	ArrayList<Members> findByDeptid(Depts deptid);
 	ArrayList<Members> findByJoblv(int joblv);
+	
+	@Transactional
+	@Modifying
+	@Query(value="update dataroom set cnt=cnt+1 where num=:num",nativeQuery = true)
+	void updateCnt(@Param("num") int num);
 }
