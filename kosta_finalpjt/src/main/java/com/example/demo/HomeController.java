e7ef279c0ecb386cb56e5cc6c81a6f88c0a57955
choiwanmin <choi.wm512@gmail.com>
2024-06-17 12:42:04 +0900

    refactor:homecontroller 코드 수정

--- kosta_finalpjt/src/main/java/com/example/demo/HomeController.java
+++ kosta_finalpjt/src/main/java/com/example/demo/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.charts.ChartsService;
+import com.example.demo.members.MembersDto;
 import com.example.demo.members.MembersService;
 import com.example.demo.users.UsersService;
 
@@ -52,12 +53,31 @@ public class HomeController {
 	}
 
 	@RequestMapping("/index_admin")
-	public void adminHome() {
+	public void adminHome(ModelMap map) {
 
 	}
 
 	@RequestMapping("/index_emp")
-	public void empHome(ModelMap map) {
+	public void empHome(HttpSession session, ModelMap map) {
+		map.addAttribute("usernm", uservice.getById((String) session.getAttribute("loginId")));
+		MembersDto mdto = mservice.getByuserId((String) session.getAttribute("loginId"));
+		if (mdto != null) {
+			if (mdto.getMemberimgnm() == "") {
+				session.setAttribute("memberimgnm", mdto.getMemberimgnm());
+			} else {
+				session.setAttribute("memberimgnm", "Default image");
+			}
+			if (mdto.getDeptid() == null) {
+				session.setAttribute("deptnm", "미등록 상태");
+			} else {
+				session.setAttribute("deptnm", mdto.getDeptid().getDeptnm());
+			}
+			if (mdto.getJoblvid() == null) {
+				session.setAttribute("joblvnm", "미등록 상태");
+			} else {
+				session.setAttribute("joblvnm", mdto.getJoblvid().getJoblvnm());
+			}
+		}
 		map.addAttribute("list", chartsService.getAll());
 	}
 
