cba0f670d57b273d3053b5d236a7a513952d6432
JinHaGyung <wlsgkrud96@gmail.com>
2024-06-18 12:42:09 +0900

    fix:record dao

--- kosta_finalpjt/src/main/java/com/example/demo/workinoutrecords/WorkInOutRecordController.java
+++ kosta_finalpjt/src/main/java/com/example/demo/workinoutrecords/WorkInOutRecordController.java
@@ -125,7 +125,7 @@ public class WorkInOutRecordController {
         if(hours>=9) {
         	hours--;
         }
-        String worktime = String.format("%d:%02d", hours, minutes);
+        String worktime = String.format("%02d:%02d", hours, minutes);
         
         w.setState(type);
         w.setWorkHours(worktime);
