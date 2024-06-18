9fed6959b4559696cf1ea41035c24e0a6ab12ca2
idgod8451 <idgod8451@gmail.com>
2024-06-18 16:39:42 +0900

    Docx:(06.18)Update:문서양식 추가에 따른 Vo formcontroller 수정

--- kosta_finalpjt/src/main/java/com/example/demo/docx/DocxDao.java
+++ kosta_finalpjt/src/main/java/com/example/demo/docx/DocxDao.java
@@ -9,18 +9,19 @@ import com.example.demo.users.Users;
 
 @Repository
 public interface DocxDao extends JpaRepository<Docx, Integer> {
+	//writer 검색
 	List<Docx> findDistinctByWriter (Users writer);
+	//formtype으로 검색 num 값으로 정렬후 출력
 	List<Docx> findByFormtypeOrderByFormnumDesc(String formtype);
+	//title like 검색
 	List<Docx> findByTitleLike(String title);
-	
 	//docxkey로 검색해서 그 문서안에 전체 시니어 검색하는 메서드
 	List<Docx> findByDocxkeyAndFormtype(int docxkey, String formtype);
-	
+	//docxkey값을 기준으로 중복되는 문서 정리해주는 메서드
 	List<Docx> findDistinctByDocxkey(int docxkey);
-	
 	//중복제거 해서 전체 리스트 가져오기
 	List<Docx> findByDocxorder(int num);
-	
+	//Senior이름으로 검색
 	List<Docx> findBySenior(String senior);
 	
 }
