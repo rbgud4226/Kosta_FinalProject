47cbddd92eda8f9bd1d5cc66ff0e97700344b08c
JinHaGyung <wlsgkrud96@gmail.com>
2024-06-24 11:49:39 +0900

    feat:record 더미데이터 생성

--- kosta_finalpjt/src/main/java/com/example/demo/oracledb/workinoutrecords/ChartDeptMember.java
+++ kosta_finalpjt/src/main/java/com/example/demo/oracledb/workinoutrecords/ChartDeptMember.java
@@ -11,6 +11,8 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+
+//부서장용 월별 직원 근무 기록
 public class ChartDeptMember {
 	//사원번호 이름 부서번호 직급레벨 총_출근횟수 지각횟수 총_근무시간 추가근무 시간
 	private int id;
