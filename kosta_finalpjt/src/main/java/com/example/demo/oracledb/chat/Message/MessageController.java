acb737360caac0fa56629ef1011c735604976837
rbgud4226 <hkh202857@gmail.com>
2024-06-25 11:26:53 +0900

    refactor:채팅메세지 관련 불필요한 코드 삭제 및 수정

--- kosta_finalpjt/src/main/java/com/example/demo/oracledb/chat/Message/MessageController.java
+++ kosta_finalpjt/src/main/java/com/example/demo/oracledb/chat/Message/MessageController.java
@@ -1,9 +1,6 @@
 package com.example.demo.oracledb.chat.Message;
-import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,19 +16,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.demo.oracledb.chat.Room.ChatRoomService;
-import com.example.demo.oracledb.users.UsersService;
-
 @Controller
 public class MessageController {
 	@Autowired
 	private MessageService messageService;
-
-	@Autowired
-	private ChatRoomService chatRoomService;
-	
-	@Autowired
-	private UsersService usersService;
 	
 	@Autowired
     private SimpMessagingTemplate messagingTemplate;
@@ -42,20 +30,13 @@ public class MessageController {
 	@MessageMapping("/chat/message/{roomId}")
 	public void sendMessage(@Payload MessageDto chatMessage, @DestinationVariable String roomId) {
 		if(chatMessage.getType().equals("OUT")) {
-			String osg = chatRoomService.getOutChatRoom(roomId, chatMessage.getSender());
-			String partN = usersService.getById2(chatMessage.getSender()).getUsernm(); 
-			chatMessage.setContent(osg);
-			chatMessage.setPartid(partN);
-			chatMessage.setSendDate(chatRoomService.createSendDate());
+			messageService.outTypeMessage(chatMessage, roomId);
 			messageService.save(chatMessage, roomId);
 			ArrayList<MessageDto> list = messageService.getMessageByRoomId(roomId);
 	        messagingTemplate.convertAndSend("/room/" + roomId, list);
 	        messagingTemplate.convertAndSend("/recent/update", roomId);
 		} else if (chatMessage.getType().equals("FILE")) {
-	        String wpath = "http://localhost:8081/files/" + chatMessage.getFileName();
-	        chatMessage.setFileRoot(wpath);
-	        chatMessage.setFileId(UUID.randomUUID().toString());
-	        chatMessage.setContent("FILE");
+			messageService.fileTypeMessage(chatMessage, roomId);
 	        messageService.save(chatMessage, roomId);
 	        ArrayList<MessageDto> list = messageService.getMessageByRoomId(roomId);
 	        messagingTemplate.convertAndSend("/room/" + roomId, list);
@@ -83,18 +64,6 @@ public class MessageController {
 	@PostMapping("/chat/message/upload")
 	@ResponseBody
 	public Map<String, String> FileUpload(@RequestParam("file") MultipartFile file) {
-		try {
-			String originalFilename = file.getOriginalFilename();
-			String fileRoot = path + "/" + originalFilename;
-			File newFile = new File(fileRoot);
-			file.transferTo(newFile);
-			Map<String, String> response = new HashMap<>();
-			String wpath = "http://localhost:8081/files/" + originalFilename;
-			response.put("fileName", originalFilename);
-			response.put("fileRoot", wpath);
-			return response;
-		} catch (Exception e) {
-			throw new RuntimeException("파일 업로드 실패: " + e.getMessage());
-		}
+		return messageService.FileuploadMethod(file);
 	}
 }
