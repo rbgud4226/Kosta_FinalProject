fef4de60f556bfb46b498f2616ca5d2de5843f44
youn <johnch941@gmail.com>
2024-06-26 17:46:10 +0900

    update : mail 발송, 확인 기능 추가

--- kosta_finalpjt/src/main/java/com/example/demo/mariadb/mail/mail_service.java
+++ kosta_finalpjt/src/main/java/com/example/demo/mariadb/mail/mail_service.java
@@ -6,6 +6,7 @@ import com.example.demo.mariadb.users.virtual_users_dto;
 import com.example.demo.mariadb.users.virtual_users_service;
 import jakarta.mail.*;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 import jakarta.mail.search.MessageIDTerm;
 import jakarta.mail.search.SearchTerm;
 import org.apache.tomcat.util.http.fileupload.FileItem;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -31,10 +33,21 @@ public class mail_service {
   @Autowired
   private mailSenderFactory mailSenderFactory;
 
+  @Async
   public void sendMail(String loginid, String title, String content, String ref, String receiver){
+    System.out.println("********************************");
+    System.out.println("loginid : " + loginid);
+    System.out.println("title : " + title);
     virtual_users user = service.getbybox(loginid);
-    virtual_users receivd_user = service.getbybox(receiver);
+    String recievemail = null;
+    if (receiver.contains("@")){
+      recievemail = receiver;
+    } else {
+      recievemail = service.getbybox(receiver).getEmail();
+    }
     virtual_users ref_user = service.getbybox(ref);
+    System.out.println("reciever : " + recievemail);
+    System.out.println("********************************");
 
     JavaMailSender emailSender = mailSenderFactory.getSender(user.getEmail(), "1234");
 
@@ -47,13 +60,12 @@ public class mail_service {
       helper.setFrom(user.getEmail());
 //      helper.setCc(ref_user.getEmail());
       // set to 에 문자열 사용 가능
-      helper.setTo(receivd_user.getEmail());
+      helper.setTo(recievemail);
 
       // 파일 첨부
 //      File file = new File("");
 //      FileItem fileItem = new DiskFileItem("mainFile", Files.probeContentType(file.toPath()),
 //          false, file.getName(), (int) file.length(), file.getParentFile());
-
 
       emailSender.send(message);
     } catch (MessagingException e) {
@@ -90,7 +102,7 @@ public class mail_service {
         rmail.put("To", message.getAllRecipients()[0]);
         rmail.put("Format-Date", formatter.format(message.getSentDate()));
         rmail.put("Date", message.getSentDate());
-        rmail.put("Content", message.getContent());
+        rmail.put("Content", getEmailContent(message.getContent()));
         maillist.add(rmail);
       }
 
@@ -116,10 +128,11 @@ public class mail_service {
     properties.put("mail.store.protocol","imap");
     Session session = Session.getDefaultInstance(properties);
     Map<String, Object> rmail = new HashMap<>();
+    SimpleDateFormat formatter = new SimpleDateFormat("MM-dd HH:mm");
 
     try{
       Store store = session.getStore("imap");
-      store.connect(user.getEmail(), user.getPassword());
+      store.connect(user.getEmail(), "1234");
 
       Folder inbox = store.getFolder("INBOX");
       inbox.open(Folder.READ_WRITE);
@@ -129,10 +142,13 @@ public class mail_service {
       Message[] message = inbox.search(searchTerm);
       rmail.put("Message-ID",message[0].getHeader("Message-ID")[0]);
       rmail.put("Subject", message[0].getSubject());
-      rmail.put("From", message[0].getFrom()[0]);
+      String from = String.valueOf(message[0].getFrom()[0]).split("@")[0];
+      rmail.put("From", from);
+      rmail.put("From-mail", message[0].getFrom()[0]);
       rmail.put("To", message[0].getAllRecipients()[0]);
+      rmail.put("Format-Date", formatter.format(message[0].getSentDate()));
       rmail.put("Date", message[0].getSentDate());
-      rmail.put("Content", message[0].getContent());
+      rmail.put("Content", getEmailContent(message[0].getContent()));
 
       inbox.close();
       store.close();
@@ -179,5 +195,38 @@ public class mail_service {
     } catch (MessagingException e) {
       e.printStackTrace();
     }
+  }
+  private String getEmailContent(Object content) throws MessagingException, IOException {
+    String result = "";
+    if (content instanceof MimeMultipart) {
+      result = getTextFromMimeMultipart((MimeMultipart) content);
+    } else if (content instanceof String) {
+      result = (String) content;
+    } else {
+      // 기타 경우 처리
+      result = content.toString();
+    }
+    return result;
+  }
+
+  private String getTextFromMimeMultipart(MimeMultipart mimeMultipart) throws MessagingException, IOException {
+    StringBuilder result = new StringBuilder();
+    int count = mimeMultipart.getCount();
+    for (int i = 0; i < count; i++) {
+      BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+      String disposition = bodyPart.getDisposition();
+
+      if (disposition != null && (disposition.equalsIgnoreCase(Part.ATTACHMENT))) {
+        continue; // 첨부 파일은 무시
+      } else if (bodyPart.isMimeType("text/plain")) {
+        result.append(bodyPart.getContent());
+      } else if (bodyPart.isMimeType("text/html")) {
+        String html = (String) bodyPart.getContent();
+        result.append(org.jsoup.Jsoup.parse(html).text());
+      } else if (bodyPart.getContent() instanceof MimeMultipart) {
+        result.append(getTextFromMimeMultipart((MimeMultipart) bodyPart.getContent()));
+      }
+    }
+    return result.toString();
   }
 }
--- kosta_finalpjt/src/main/resources/static/js/mailserver.js
+++ kosta_finalpjt/src/main/resources/static/js/mailserver.js
@@ -1,15 +1,29 @@
 $(document).ready(function (){
 
-    function maillist(){
-        $.ajax({
-            url:"/mail/list",
-            dataType: 'json',
-            method: 'get',
-            data:{
+    $('.mail_btn').on('click', function (){
+        var tit = document.getElementsByName("title");
+        var con = document.getElementsByName("content");
+        var rf = document.getElementsByName("ref");
+        var rr = document.getElementsByName("receiver");
 
-            }
-        }).done(function (list){
-
-        })
-    }
+       $.ajax({
+           url:"/mail/sendmail",
+           dataType:'json',
+           method:'post',
+           data:{
+               title : tit,
+               content : con,
+               ref : rf,
+               receiver : rr
+           },
+           success: function (response) {
+               // 성공 시 처리할 내용
+               console.log("Mail sent successfully:", response);
+           },
+           error: function (error) {
+               // 에러 시 처리할 내용
+               console.error("Error sending mail:", error);
+           }
+       }) ;
+    });
 });
